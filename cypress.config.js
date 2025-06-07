9922025e737dc11d5e620f1220b8a69e396a6bb8
ITALO OLIVEIRA <i.seguros10@gmail.com>
2025-06-07 09:37:12 -0300

    configure cypress cloud

--- cypress.config.js
+++ cypress.config.js
@@ -3,5 +3,10 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   viewportHeight: 880,
   viewportWidth: 1280,
-  e2e: {},
+  projectId: 'ok5pqh', // <- está no mesmo nível de e2e
+  e2e: {
+    setupNodeEvents(on, config) {
+      // eventos customizados (se precisar depois)
+    },
+  },
 })
